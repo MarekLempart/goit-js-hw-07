711a6b5603ccbbdc31129b38d26b66fe55202ec2
MarekLempart <kama.zakupy@gmail.com>
2024-01-24 14:19:02 +0100

    początek pracy

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+// galleryList.addEventListener("clikck", selectImage);
+
+// function selectImage(event) {
+
+//   if (event.target.nodeName === "IMG") {
+//     const instance = basicLightbox.create(
+//         <img src="${event.target.dataset.source}" alt="${event.target.alt}">
+//     );
+//     instance.show();
+//   }
+// });
+
+// console.log(galleryItems);
+
+// import galleryItems from "./gallery-items.js";
+
+// const galleryList = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+  const listItem = document.createElement("li");
+  const link = document.createElement("a");
+  const image = document.createElement("img");
+
+  listItem.classList.add("gallery__item");
+  link.classList.add("gallery__link");
+  image.classList.add("gallery__image");
+
+  link.href = item.original;
+  image.src = item.preview;
+  image.dataset.source = item.original;
+  image.alt = item.description;
+
+  link.appendChild(image);
+  listItem.appendChild(link);
+  galleryList.appendChild(listItem);
+});
+
+galleryList.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (event.target.nodeName === "IMG") {
+    const instance = basicLightbox.create(`
+      <img src="${event.target.dataset.source}" alt="${event.target.alt}">
+    `);
+
+    instance.show();
+  }
+});
