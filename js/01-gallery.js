7501a3d07bf58d8d47e2c020b9227242ead04b03
MarekLempart <kama.zakupy@gmail.com>
2024-01-25 15:21:46 +0100

    Praca nad zadaniem 07 część pierwsza

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,31 +1,13 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 const galleryList = document.querySelector(".gallery");
-// galleryList.addEventListener("clikck", selectImage);
 
-// function selectImage(event) {
-
-//   if (event.target.nodeName === "IMG") {
-//     const instance = basicLightbox.create(
-//         <img src="${event.target.dataset.source}" alt="${event.target.alt}">
-//     );
-//     instance.show();
-//   }
-// });
-
-// console.log(galleryItems);
-
-// import galleryItems from "./gallery-items.js";
-
-// const galleryList = document.querySelector(".gallery");
-
-galleryItems.forEach((item) => {
-  const listItem = document.createElement("li");
+const galleryElements = galleryItems.map((item) => {
+  const galleryListItem = document.createElement("li");
   const link = document.createElement("a");
   const image = document.createElement("img");
 
-  listItem.classList.add("gallery__item");
+  galleryListItem.classList.add("gallery__item");
   link.classList.add("gallery__link");
   image.classList.add("gallery__image");
 
@@ -35,58 +17,105 @@ galleryItems.forEach((item) => {
   image.alt = item.description;
 
   link.appendChild(image);
-  listItem.appendChild(link);
-  galleryList.appendChild(listItem);
+  galleryListItem.appendChild(link);
+  return galleryListItem;
 });
+
+galleryList.append(...galleryElements);
 
 galleryList.addEventListener("click", (event) => {
   event.preventDefault();
-
   if (event.target.nodeName === "IMG") {
     const instance = basicLightbox.create(`
-      <img src="${event.target.dataset.source}" alt="${event.target.alt}">
+      <img src="${event.target.dataset.source}" alt="${event.target.alt}" width="800" height="600">
     `);
-
     instance.show();
   }
 });
 
-// import { galleryItems } from "./gallery-items.js";
-// const gallery = document.querySelector(".gallery");
+document.addEventListener("keydown", (event) => {
+  const instance = basicLightbox.instance();
+  if (event.key === "Escape" && instance.visible()) {
+    instance.close();
+  }
+});
 
-// document.body.addEventListener("keypress", (e) => {
-//   if (e.key === "Escape") basicLightbox.close();
+// Change code below this line
+console.log(galleryItems);
+
+// import { galleryItems } from "./gallery-items.js";
+// // Change code below this line
+// const galleryList = document.querySelector(".gallery");
+
+// galleryItems.forEach((item) => {
+//   const galleryListItem = document.createElement("li");
+//   const link = document.createElement("a");
+//   const image = document.createElement("img");
+
+//   galleryListItem.classList.add("gallery__item");
+//   link.classList.add("gallery__link");
+//   image.classList.add("gallery__image");
+
+//   link.href = item.original;
+//   image.src = item.preview;
+//   image.dataset.source = item.original;
+//   image.alt = item.description;
+
+//   link.appendChild(image);
+//   galleryListItem.appendChild(link);
+//   galleryList.appendChild(galleryListItem);
 // });
 
-// for (let item of galleryItems) {
-//   const galleryItem = document.createElement("div");
-//   galleryItem.classList.add("gallery__item");
-
-//   const galleryLink = document.createElement("a");
-//   galleryLink.classList.add("gallery__link");
-//   galleryLink.href = item.original;
-
-//   const galleryImage = document.createElement("img");
-//   galleryImage.classList.add("gallery__image");
-//   galleryImage.src = item.preview;
-//   galleryImage.dataset.source = item.original;
-//   galleryImage.alt = item.description;
-
-//   galleryLink.appendChild(galleryImage);
-//   galleryItem.appendChild(galleryLink);
-//   gallery.appendChild(galleryItem);
-// }
-
-// gallery.addEventListener("click", (event) => {
+// galleryList.addEventListener("click", (event) => {
 //   event.preventDefault();
+//   if (event.target.nodeName === "IMG") {
+//     const instance = basicLightbox.create(`
+//       <img src="${event.target.dataset.source}" alt="${event.target.alt}" width="800" height="600">
+//     `);
+//     instance.show();
+//   }
+// });
 
-//   const lightbox = basicLightbox.create(`
-//     <img src="${event.target.dataset.source}" width="800" height="600">
-//   `);
-
-//   lightbox.show();
+// document.addEventListener("keydown", (event) => {
+//   const instance = basicLightbox.instance();
+//   if (event.key === "Escape" && instance.visible()) {
+//     instance.close();
+//   }
 // });
 
 // // Change code below this line
+// console.log(galleryItems);
 
+// import { galleryItems } from "./gallery-items.js";
+// const galleryList = document.querySelector(".gallery");
+
+// for (let item of galleryItems) {
+//   const galleryListItem = document.createElement("div");
+//   const link = document.createElement("a");
+//   const image = document.createElement("img");
+
+//   galleryListItem.classList.add("gallery__item");
+//   link.classList.add("gallery__link");
+//   image.classList.add("gallery__image");
+
+//   link.href = item.original;
+//   image.src = item.preview;
+//   image.dataset.source = item.original;
+//   image.alt = item.description;
+
+//   link.appendChild(image);
+//   galleryListItem.appendChild(link);
+//   galleryList.appendChild(galleryListItem);
+// }
+
+// galleryList.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   if (event.target.nodeName === "IMG") {
+//     const instance = basicLightbox.create(`
+//       <img src="${event.target.dataset.source}" alt="${event.target.alt}" width="800" height="600">
+//     `);
+//     instance.show();
+//   }
+// });
+// // Change code below this line
 // console.log(galleryItems);
